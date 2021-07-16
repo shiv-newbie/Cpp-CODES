772a3dd3cc46826f7bdca4c634301296a5a8d76a
shiv-newbie <67487860+shiv-newbie@users.noreply.github.com>
2021-07-16 16:17:39 +0800

    Create Morris Algorithm Binary Traversal.cpp
    Morris Algorithm for inorder binary traversal

---
+++ Algorithms/Morris Algorithm Binary Traversal.cpp
@@ -0,0 +1,73 @@
+// Morris Algorithm for inorder binary traversal
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <math.h>
+#include <limits.h>
+#include <string.h>
+#include <set>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+   
+};
+
+int main()
+{
+    
+    TreeNode* root = new TreeNode;
+    TreeNode* current = root;
+    root->val = 1;
+    root->left = new TreeNode;
+    root->right = new TreeNode;
+    root->left->val = 2;
+    root->right->val = 3;
+    root->left->left = new TreeNode;
+    root->left->right = new TreeNode;
+    root->left->left->val = 4;
+    root->left->right->val = 5;
+    
+
+    while(current != NULL)
+    {
+        if(!current->left)
+        {
+            cout << current->val << " ";
+            current = current->right;
+        }
+        else
+        {
+            TreeNode* n = current->left;
+            while(n->right && n->right != current)
+                n = n->right;
+
+            if(n->right == NULL)
+            {
+                n->right = current;
+                current = current->left;
+            }
+            else
+            {
+                n->right = NULL;
+                cout << current->val << " ";
+                current = current->right;
+            }
+        }
+    }
+    
+    return 0;
+}
